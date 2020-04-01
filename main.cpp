c6b22019d79e724aea113282439bb6fc7a887e0b
xarowsky <wiktorch@vp.pl>
2020-04-01 20:17:37 +0200

    v0.0000001

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-int main(void)
+struct wezel{
+
+    int data;
+    wezel *next[];
+
+};
+
+class Bdrzewo{
+
+    private:
+        wezel *wierzcholek=NULL;
+        int rzad=10;
+
+    public:
+        void inicjalizacja(){
+
+            cout<<"Podaj rzad drzewa:"<<endl;
+
+            while(rzad>5) cin>>rzad;
+
+        }
+
+        void dodaj(){
+
+        }
+
+        void szukaj(){
+
+
+        }
+};
+
+
+
+
+
+int main()
 {
-	
-	return 0;
+    Bdrzewo tree;
+    tree.inicjalizacja();
+    return 0;
 }
